8aa89cac89503d456abd90ff08fdf65b4a30059a
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-04-01 09:35:19 -0500

    Created paddle dynamic behavior

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -97,6 +97,30 @@ import GameplayKit
             addChild(loseZone)
         }
         
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for touch in touches {
+                let location = touch.location(in: self)
+                paddle.position.x = location.x
+            }
+        }
         
+        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for touch in touches {
+                let location = touch.location(in: self)
+                paddle.position.x = location.x
+            }
+        }
+        
+        func didBegin(_ contact: SKPhysicsContact) {
+            if contact.bodyA.node?.name == "brick" || contact.bodyB.node?.name == "brick" {
+                print("You win!")
+                brick.removeFromParent()
+                ball.removeFromParent()
+            }
+            if contact.bodyA.node?.name == "loseZone" || contact.bodyB.node?.name == "LoseZone" {
+                print("You lose!")
+                ball.removeFromParent()
+            }
+        }
     }
     
