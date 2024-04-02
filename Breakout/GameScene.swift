50e92e2ad0672b3c278080e8a9847cd6d16a7b53
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-04-02 10:08:39 -0500

    Set up game over routine

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -161,13 +161,22 @@ import GameplayKit
         
         func didBegin(_ contact: SKPhysicsContact) {
             if contact.bodyA.node?.name == "brick" || contact.bodyB.node?.name == "brick" {
-                print("You win!")
-                brick.removeFromParent()
-                ball.removeFromParent()
+                gameOver(winner: true)
             }
             if contact.bodyA.node?.name == "loseZone" || contact.bodyB.node?.name == "LoseZone" {
-                print("You lose!")
-                ball.removeFromParent()
+                gameOver(winner: false)
+            }
+        }
+        
+        func gameOver(winner: Bool) {
+            playingGame = false
+            playLabel.alpha = 1
+            resetGame()
+            if winner {
+                playLabel.text = "You win! Tap to play again"
+            }
+            else {
+                playLabel.text = "You lose! Tap to play again"
             }
         }
     }
